03e73e6f8c57d8203bf1d3c3f19c6d8aee0ca3e8
ccampos <juan.cherrera@academicos.udg.mx>
2024-11-27 18:23:37 -0600

    Add files via upload

---
+++ 1. Fundamentos/multiplica_spark.py
@@ -0,0 +1,22 @@
+#Esta es un buen ejercicio que nos enseña una funcion del spark
+
+
+import findspark
+findspark.init()
+from pyspark import SparkContext
+
+# Crea un contexto Spark
+sc = SparkContext("local", "EjemploRDD")
+#sc=  SparkContext(appName="MyAppName")
+
+# Crea un RDD a partir de una lista
+rdd = sc.parallelize([1, 2, 3, 4, 5])
+
+# Realiza una transformación: multiplica cada elemento por 2
+rdd_multiplicado = rdd.map(lambda x: x * 2)
+
+# Realiza una acción: muestra los resultados
+print(rdd_multiplicado.collect())
+#rdd_multiplicado.collect()
+# Detén el contexto Spark
+# sc.stop()
